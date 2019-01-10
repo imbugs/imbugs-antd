84ab52a16ca6ab60076525cd3c0199ad6e0a4d71
imbugs <imbugs@126.com>
2019-01-11 00:52:50 +0800

    支持onRender

--- src/component/DynamicTableForm.js
+++ src/component/DynamicTableForm.js
@@ -1,11 +1,14 @@
+import map from "lodash/map";
 import React from "react";
 import { Button, Form, Icon, Input, Table } from "antd";
 
 /**
  * 动态表单
+ * @param key react的key数据, 可设置key={type}, 当type变动的时候table重新挂载，如果需要重设table可设置此项
  * @param columnsInfo 列信息
  * @param tableInfo 表信息
  * @param onChange 更新内容事件
+ * @param onRender 在render的时候回调修改dataSource内容, 可为每一行添加disable参数
  */
 class DynamicTableForm extends React.Component {
   columnsInfo = this.props.columnsInfo || [{
@@ -26,11 +29,28 @@ class DynamicTableForm extends React.Component {
     dataSource: [],
   };
 
-  initDataSource = () => {
-    /** 上层props.dataSource变更时如果当前组件 */
-    if (!this.changed && (this.state.dataSource == null || this.state.dataSource.length == 0)) {
-      this.state.dataSource = this.props.dataSource && (Array.isArray(this.props.dataSource) && this.props.dataSource || [this.props.dataSource]) || [{}]
+
+  doUpdate = (change = true) => {
+    this.changed = change;
+    let data = [];
+    for (let i in this.state.dataSource) {
+      if (!this.isEmptyRow(this.state.dataSource[i])) {
+        data.push(this.state.dataSource[i]);
+      }
     }
+    this.cb && this.cb(data);
+  };
+
+  onRender = () => {
+    if (typeof (this.props.onRender) === 'function') {
+      let updateDataSource = this.props.onRender(this.state.dataSource);
+      this.state.dataSource = Array.isArray(updateDataSource) ? updateDataSource : this.state.dataSource;
+    }
+  };
+
+  onChange = (e, record, field) => {
+    record[field] = e.target.value;
+    this.setState({}, this.doUpdate)
   };
 
   isEmptyRow = (rowData) => {
@@ -43,23 +63,6 @@ class DynamicTableForm extends React.Component {
     return true
   };
 
-  doUpdate = () => {
-    this.changed = true;
-    let data = [];
-    for (let i in this.state.dataSource) {
-      if (!this.isEmptyRow(this.state.dataSource[i])) {
-        data.push(this.state.dataSource[i]);
-      }
-    }
-    this.cb && this.cb(data);
-  };
-
-  onChange = (e, record, field) => {
-    record[field] = e.target.value;
-    this.setState({}, this.doUpdate)
-  };
-
-
   add = () => {
     this.setState({ dataSource: [...this.state.dataSource, {}] }, this.doUpdate)
   };
@@ -68,22 +71,34 @@ class DynamicTableForm extends React.Component {
     this.setState({ dataSource: this.state.dataSource.filter(e => e !== record) }, this.doUpdate)
   };
 
-  componentDidMount = () => {
+  componentDidMount() {
     // 初始化完成时触发一次数据更新
-    this.doUpdate();
+    this.doUpdate(false);
   };
+
+  componentWillReceiveProps(nextProps, nextContent) {
+    /**
+     * 上层props.dataSource变化时，当前表格无变动且表格为空时可接收props数据
+     * 作用：即对props变化保持敏感, 又防止出现dataSource地址相同时产生的问题
+     */
+    if (!this.changed && (!this.state.dataSource || this.state.dataSource.length == 0)) {
+      let dataSource = nextProps.dataSource && (Array.isArray(nextProps.dataSource) && nextProps.dataSource || [nextProps.dataSource]) || [{}]
+      this.state.dataSource = map(dataSource, item => ({ ...item }));
+      this.doUpdate(false);
+    }
+  }
 
   columns = (() => {
     var merge = [];
     for (var i in this.columnsInfo) {
-      var item = this.columnsInfo[i];
-      const { title, key } = item;
+      var columnItem = this.columnsInfo[i];
+      const { title, key } = columnItem;
       var render = record => {
         return <Input placeholder={`请填写${title}`} value={record[key]}
-                      onChange={(e) => this.onChange(e, record, key)}/>
+                      onChange={(e) => this.onChange(e, record, key)} disabled={record.disabled}/>
       };
       merge.push({
-        ...item,
+        ...columnItem,
         render
       });
     }
@@ -93,15 +108,15 @@ class DynamicTableForm extends React.Component {
       width: 60,
       align: 'right',
       render: record => {
-        return <a onClick={(e) => this.del(record)}>删除</a>
+        return record.disabled ? null : <a onClick={(e) => this.del(record)}>删除</a>
       }
     });
     return merge;
   })();
 
   render() {
-    this.initDataSource();
     let rowKey = 0;
+    this.onRender();
     return (
       <div>
         <Table
